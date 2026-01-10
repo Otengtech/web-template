3666f981a2ab71c4a6a38750a1d0328d770a90fe
Otengtech <otengebenezer326@gmail.com>
2026-01-09 18:52:33 -0800

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Poppins } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import Navbar from "@/app/Components/Navbar";
 import HomePage from "@/app/page";
 import Footer from "./Components/Footer";
